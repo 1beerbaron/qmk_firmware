31298fa7a78933ee9e2ba04aee0b9bca86df6ded
1beerbaron <tylerbuehler@gmail.com>
2024-09-08 19:34:16 -0400

    Update keymap.c
    clarify comment

--- keyboards/keyhive/navi10/keymaps/1beerbaron/keymap.c
+++ keyboards/keyhive/navi10/keymaps/1beerbaron/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
                             KC_UP,
                  KC_LEFT,   KC_DOWN,    KC_RIGHT),
-    [_FN0] = LAYOUT(/* function layer */
+    [_FN0] = LAYOUT(/* function layer, press and hold*/
                  KC_TRNS,   LALT(KC_F4),    KC_VOLU,
                  KC_ENTER,  KC_SCRL,    KC_VOLD,
 
